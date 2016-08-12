bf240743aebb70e003d4b4324fd2c52c1a857707
Ruman Gerst <ruman.gerst@uni-jena.de>
2016-08-12 03:16:49 +0200

    + Added street base tile that automatically connects + Added silo model + Now preventing exploiting foundations as streets (now buildings can be non-connective)

--- BuildingGame3DClient/Game/create_building.py
+++ BuildingGame3DClient/Game/create_building.py
@@ -1,10 +1,14 @@
 #!/bin/env python
 
+import math
+
 name = input("Name: ")
 description = input("Description: ")
 typename = input("Typename: ")
 category = input("Category: ")
+erosion_type = input("Erosion type: ")
 
+res_ = {}
 res_per_progress = {}
 
 while True:
@@ -15,16 +19,29 @@ while True:
     if not res or not amount:
         break
     
-    res_per_progress[res] = amount
+    res_[res] = int(amount)
     
-progress = input("Progress per resource: ")
+progress_duration = int(input("Build duration: "))
+
+while 100 % progress_duration != 0:
+    print("Invalid! 100 must be dividable by this number!")
+    progress_duration = int(input("Build duration: "))
+
+progress = 100 / progress_duration
+
+for k in res_:
+    
+    res_per_progress[k] = str(int(math.ceil(float(res_[k]) / float(progress_duration))))
+    print(k + " per progress: " + str(res_per_progress[k]))
 
 f = open("template.json", "r")
 w = open("definitions/building-" + category + "-" + typename + ".json", "w")
 
+progress = str(progress)
+
 for line in f:
     
-    line = line.replace("%NAME", name).replace("%DESCR", description).replace("%TYPE", typename).replace("%CATEGORY", category).replace("%PROGRESS_AMOUNT", progress).replace("%RESOURCES_PER_PROGRESS", ", ".join( [ "\"" + k + "\" : " + res_per_progress[k]  for k in res_per_progress ] ))
+    line = line.replace("%NAME", name).replace("%DESCR", description).replace("%TYPE", typename).replace("%CATEGORY", category).replace("%PROGRESS_AMOUNT", progress).replace("%RESOURCES_PER_PROGRESS", ", ".join( [ "\"" + k + "\" : " + res_per_progress[k]  for k in res_per_progress ] )).replace("%EROSION_T", erosion_type)
     w.write(line)
 
 w.close()
